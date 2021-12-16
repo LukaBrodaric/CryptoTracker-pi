abe492b371f0e5a7cd2a507370bb05e988f19b8a
AlenOrbanic <aorbanic@student.unipu.hr>
2021-12-16 17:57:33 +0100

    homepage commit

--- src/views/News.vue
+++ src/views/News.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="about">
+  <div>
     <h1>This is an news page</h1>
   </div>
 </template>
--- src/views/Portfolio.vue
+++ src/views/Portfolio.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="about">
+  <div>
     <h1>This is an portoflio page</h1>
   </div>
 </template>
