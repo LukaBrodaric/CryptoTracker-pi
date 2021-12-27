f9cde776315883ad6152516cdd20c492fb23b06f
unknown <brodaricluka81@gmail.com>
2021-12-27 22:11:03 +0100

    Dodan landing page

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,20 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Home from "../views/Home.vue";
+import Landingpage from "../views/Landingpage.vue";
 
 const routes = [
   {
     path: "/",
+    name: "Landingpage",
+    component: Landingpage,
+  },
+  {
+    path: "/Home",
     name: "Home",
-    component: Home,
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/Home.vue"),
   },
   {
     path: "/portfolio",
