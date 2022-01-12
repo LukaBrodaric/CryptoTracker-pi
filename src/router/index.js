4adfdb8171a7df596ae5c669dd2749e9d4d23bf7
AlenOrbanic <aorbanic@student.unipu.hr>
2022-01-12 16:32:23 +0100

    Brisanje logina
    Registracija i login se oboje nalaze na "signup" ruti.

--- src/router/index.js
+++ src/router/index.js
@@ -33,15 +33,6 @@ const routes = [
     },
   },
   {
-    path: "/login",
-    name: "Login",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/Login.vue"),
-  },
-  {
     path: "/signup",
     name: "Signup",
     // route level code-splitting
