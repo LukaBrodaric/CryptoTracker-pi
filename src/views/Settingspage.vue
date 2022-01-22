794f7a302578c2caff3a78f7f9ab035a0778ab3e
unknown <brodaricluka81@gmail.com>
2022-01-22 18:44:15 +0100

    Dodani collapsovi za Notification, Versio, ToS, Privacy Policiy i Help na settings page-u. Treba jos povezati signout i reminders-e

--- src/views/Settingspage.vue
+++ src/views/Settingspage.vue
@@ -3,9 +3,9 @@
     <div class="container currency-settings-div">
       <div class="stng-header">
         <h2>Settings</h2>
-        <a href="#" class="currency-settings">
+        <p class="currency-settings">
           <ion-icon name="settings-outline"></ion-icon>
-        </a>
+        </p>
       </div>
 
       <!-- Settings i edit profile -->
@@ -25,39 +25,128 @@
 
       <ul class="settings-navigations">
         <li>
-          <a href="#">
-            <h4>Notification</h4>
-            <ion-icon name="card-outline"></ion-icon>
+          <a
+            data-toggle="collapse"
+            href="#multiCollapseExample1"
+            role="button"
+            aria-expanded="false"
+            aria-controls="multiCollapseExample1"
+            ><div class="row">
+              <h4>Notification</h4>
+
+              <!-- <ion-icon name="card-outline"></ion-icon> -->
+
+              <div class="collapse multi-collapse" id="multiCollapseExample1">
+                <div class="mt-3">
+                  CryptoTracker allows right to send you notification about any
+                  new updas to app. If you don't want to allow notifications
+                  send email at: info@cryptotracker.com
+                </div>
+              </div>
+            </div>
           </a>
         </li>
         <li>
-          <a href="#">
-            <h4>Reminders</h4>
-            <ion-icon name="notifications-outline"></ion-icon>
+          <a
+            data-toggle="collapse"
+            href="#multiCollapseExample2"
+            role="button"
+            aria-expanded="false"
+            aria-controls="multiCollapseExample2"
+            ><div class="row">
+              <h4>Reminders</h4>
+              <!--   <ion-icon name="notifications-outline"></ion-icon> -->
+              <div class="collapse multi-collapse" id="multiCollapseExample2">
+                <div class="mt-3">Ovjde dodati remidere ako cemo imati</div>
+              </div>
+            </div>
           </a>
         </li>
         <li>
-          <a href="#">
-            <h4>Version</h4>
-            <ion-icon name="phone-portrait-outline"></ion-icon>
+          <a
+            data-toggle="collapse"
+            href="#multiCollapseExample3"
+            role="button"
+            aria-expanded="false"
+            aria-controls="multiCollapseExample3"
+            ><div class="row">
+              <h4>Version</h4>
+              <!--  <ion-icon name="phone-portrait-outline"></ion-icon> -->
+              <div class="collapse multi-collapse" id="multiCollapseExample3">
+                <div class="mt-3">
+                  CryptoTracker app is running on newest Version 2.0.6 and will
+                  be constantly updated, and you as a user will be reminded on
+                  email about any new version or updates.
+                </div>
+              </div>
+            </div>
           </a>
         </li>
         <li>
-          <a href="#">
-            <h4>Terms of Service</h4>
-            <ion-icon name="clipboard-outline"></ion-icon>
+          <a
+            data-toggle="collapse"
+            href="#multiCollapseExample4"
+            role="button"
+            aria-expanded="false"
+            aria-controls="multiCollapseExample4"
+            ><div class="row">
+              <h4>Terms of Service</h4>
+              <!--  <ion-icon name="clipboard-outline"></ion-icon> -->
+              <div class="collapse multi-collapse" id="multiCollapseExample4">
+                <div class="mt-3">
+                  These Terms of Service constitute a legally binding agreement
+                  made between you, whether personally or on behalf of an entity
+                  (“you”) and CryptoTracker. You agree that by accessing the
+                  Site, you have read, understood, and agree to be bound by all
+                  of these Terms of Service
+                </div>
+              </div>
+            </div>
           </a>
         </li>
         <li>
-          <a href="#">
-            <h4>Privacy Policy</h4>
-            <ion-icon name="shield-outline"></ion-icon>
+          <a
+            data-toggle="collapse"
+            href="#multiCollapseExample5"
+            role="button"
+            aria-expanded="false"
+            aria-controls="multiCollapseExample5"
+            ><div class="row">
+              <h4>Privacy Policy</h4>
+              <!--   <ion-icon name="shield-outline"></ion-icon> -->
+              <div class="collapse multi-collapse" id="multiCollapseExample5">
+                <div class="mt-3">
+                  CryptoTracker operates the CryptoTracker website, which
+                  provides the SERVICE.This page is used to inform website
+                  visitors regarding our policies with the collection, use, and
+                  disclosure of Personal Information if anyone decided to use
+                  our Service, the CryptoTracker website.
+                </div>
+              </div>
+            </div>
           </a>
         </li>
         <li>
-          <a href="#">
-            <h4>Help</h4>
-            <ion-icon name="code-outline"></ion-icon>
+          <a
+            data-toggle="collapse"
+            href="#multiCollapseExample6"
+            role="button"
+            aria-expanded="false"
+            aria-controls="multiCollapseExample6"
+            ><div class="row">
+              <h4>Help</h4>
+              <!--  <ion-icon name="code-outline"></ion-icon> -->
+              <div class="collapse multi-collapse" id="multiCollapseExample6">
+                <div class="mt-3">
+                  You may be required to register with the Site. You agree to
+                  keep your password confidential and will be responsible for
+                  all use of your account and password. We reserve the right to
+                  remove, reclaim, or change a username you select if we
+                  determine, in our sole discretion, that such username is
+                  inappropriate, obscene, or otherwise objectionable.
+                </div>
+              </div>
+            </div>
           </a>
         </li>
       </ul>
