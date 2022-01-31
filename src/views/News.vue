377936c52f476de10bfc5cccf0cc235834cb4134
unknown <brodaricluka81@gmail.com>
2022-01-31 16:22:27 +0100

    Implementirane vijesti na news page, treba srediti dizajn

--- src/views/News.vue
+++ src/views/News.vue
@@ -1,5 +1,14 @@
 <template>
   <div>
     <h1>This is an news page</h1>
+
+    <iframe
+      width="100%"
+      scrolling="no"
+      allowtransparency="true"
+      frameborder="0"
+      src="https://cryptopanic.com/widgets/news/?bg_color=FFFFFF&amp;currencies=BTC%2CETH%2CLTC%2CADA%2CBNB%2CSOL&amp;font_family=sans&amp;font_size=20&amp;header_bg_color=2C3E50&amp;header_text_color=FFFFFF&amp;link_color=0099FF&amp;news_feed=trending&amp;posts_limit=10&amp;text_color=2C3E50&amp;title=Trending%20news"
+      height="1000px"
+    ></iframe>
   </div>
 </template>
