97099ce8689ff24032c488ed5e6730c170972082
AlenOrbanic <aorbanic@student.unipu.hr>
2022-02-01 14:25:06 +0100

    upload image api

--- src/views/Settingspage.vue
+++ src/views/Settingspage.vue
@@ -10,17 +10,30 @@
 
       <!-- Settings i edit profile -->
 
-      <a href="#" class="settings-profile">
+      <a href="#open-modal" class="settings-profile">
         <img src="@/assets/profile.png" alt="porfile" class="profile-img" />
         <div class="right">
           <div>
             <h2>Edit Profile</h2>
             <div class="user">{{store.currentUser}}</div>
           </div>
+          <h5>Click here to change your profile picture</h5>
           <ion-icon name="chevron-forward-outline"></ion-icon>
         </div>
       </a>
-
+        <div id="open-modal" class="modal-window">
+          <div>
+            <a href="#" title="Close" class="modal-close">Close</a>
+            <br /><br />
+            <h1><b>Select an image</b></h1>
+            <UploadImages :max="1"/>
+            <div>
+             <form action="calcular.php" method="post">
+      <input type="submit" value="Upload" @click.prevent="unesiValutu()">
+    </form>
+            </div>
+          </div>
+        </div>
       <!-- Lista s navigacijom -->
 
       <ul class="settings-navigations">
@@ -156,8 +169,324 @@
   </body>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
 @import url("https://fonts.googleapis.com/css2?family=Lato&family=Nunito:wght@600&display=swap");
+
+#open-modal{
+  * {
+  box-sizing: border-box;
+}
+body {
+  color: white;
+  font-family: sans-serif;
+}
+
+form {
+  margin: auto;
+  padding: 20px;
+  margin-top: 20px;
+}
+h1 {
+  text-align: center;
+  margin-bottom: 20px;
+  margin-top: 0;
+}
+label, input,select {
+  width: 100%;
+  display: block;
+  font-size: 1.2em;
+}
+input, select {
+  padding: 5px;
+  margin-bottom: 20px;
+}
+input[type="submit"] {
+  width: 30%;
+  margin: auto;
+  background: #333;
+  color: white;
+  border: none;
+  cursor: pointer;
+}
+}
+
+.container,
+.container-fluid,
+.container-lg,
+.container-md,
+.container-sm,
+.container-xl {
+  width: 100%;
+  padding-right: 15px;
+  padding-left: 15px;
+  margin-right: auto;
+  margin-left: auto;
+}
+
+@media (min-width: 576px) {
+  .container,
+  .container-sm {
+    max-width: 540px;
+  }
+}
+
+@media (min-width: 768px) {
+  .container,
+  .container-md,
+  .container-sm {
+    max-width: 720px;
+  }
+}
+
+@media (min-width: 992px) {
+  .container,
+  .container-lg,
+  .container-md,
+  .container-sm {
+    max-width: 960px;
+  }
+}
+
+@media (min-width: 1200px) {
+  .container,
+  .container-lg,
+  .container-md,
+  .container-sm,
+  .container-xl {
+    max-width: 1140px;
+  }
+}
+
+.currency-portfolio-div {
+  position: relative;
+  margin-top: 16px;
+  font-family: "Lato", sans-serif;
+  width: 100%;
+  background-color: #ffffff;
+}
+
+.currency-portfolio-div .div-header {
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-box-align: center;
+  -ms-flex-align: center;
+  align-items: center;
+  -webkit-box-pack: justify;
+  -ms-flex-pack: justify;
+  justify-content: space-between;
+  padding: 0 16px;
+}
+
+.currency-portfolio-div .div-header h2 {
+  font-size: 28px;
+  font-weight: 700;
+  color: #17171a;
+}
+
+.currency-portfolio-div .div-header .currency-setting {
+  font-size: 20px;
+  color: #b9c1d9;
+}
+
+.currency-portfolio-div .total {
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-box-align: center;
+  -ms-flex-align: center;
+  align-items: center;
+  -webkit-box-pack: justify;
+  -ms-flex-pack: justify;
+  justify-content: space-between;
+  margin-top: 20px;
+  text-align: left;
+  margin-left: 16px;
+}
+
+.currency-portfolio-div .total h3 {
+  font-weight: 600;
+  font-size: 24px;
+  color: #000000;
+}
+
+.currency-portfolio-div .total h3:nth-child(1) {
+  -webkit-box-flex: 3;
+  -ms-flex: 3;
+  flex: 3;
+}
+
+.currency-portfolio-div .otal h3:nth-child(2) {
+  color: #093c6a;
+  -webkit-box-flex: 2;
+  -ms-flex: 2;
+  flex: 2;
+}
+
+.currency-portfolio-div .add-btn {
+  position: relative;
+  width: 100%;
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-box-pack: center;
+  -ms-flex-pack: center;
+  justify-content: center;
+  margin-top: 16px;
+  margin-bottom: 50px;
+}
+
+.currency-portfolio-div .add-btn a {
+  color: #000000;
+  font-size: 14px;
+  font-weight: 500;
+  padding: 11px;
+  width: 65%;
+  position: relative;
+  text-align: center;
+  text-decoration: none;
+  -webkit-box-shadow: 0px 3px 8px 0px #0000001f;
+  box-shadow: 0px 3px 8px 0px #0000001f;
+  border-radius: 7px;
+  border: 0.5px solid #0099ff;
+}
+
+.currency-portfolio-div .currency-div {
+  list-style: none;
+  margin-top: 16px;
+}
+
+.currency-portfolio-div .currency-div li {
+  border-bottom: 1px solid #b9c1d928;
+  margin-top: 12px;
+  padding-bottom: 12px;
+}
+
+.currency-portfolio-div .currency-div li a {
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-box-align: center;
+  -ms-flex-align: center;
+  align-items: center;
+  -webkit-box-pack: justify;
+  -ms-flex-pack: justify;
+  justify-content: space-between;
+  text-decoration: none;
+}
+
+.currency-portfolio-div .currency-div li a .left {
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-box-align: center;
+  -ms-flex-align: center;
+  align-items: center;
+  gap: 5px;
+}
+
+.currency-portfolio-div .currency-div li a p .left img {
+  height: 56px;
+  width: 56px;
+}
+
+.currency-portfolio-div .currency-div li a .left h3 {
+  font-weight: 900;
+  font-size: 16px;
+  text-transform: uppercase;
+  color: #17171a;
+  text-align: left;
+  margin-left: 5px;
+}
+
+.currency-portfolio-div .currency-div li a .left p {
+  font-weight: 400;
+  font-size: 12px;
+  color: #b9c1d9;
+  margin-top: 2px;
+  margin-left: 5px;
+  text-align: left;
+}
+
+.currency-portfolio-div .currency-div li a .right {
+  text-align: end;
+}
+
+.currency-portfolio-div .currency-div li a .right h4 {
+  font-weight: 900;
+  font-size: 16px;
+  color: #17171a;
+}
+
+.currency-portfolio-div .currency-div li a .right p {
+  font-weight: 400;
+  text-transform: uppercase;
+  font-size: 12px;
+  color: #b9c1d9;
+  margin-top: 2px;
+}
+.modal-window {
+  position: fixed;
+  background-color: #aaa9aa73;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  z-index: 999;
+  visibility: hidden;
+  opacity: 0;
+  pointer-events: none;
+  transition: all 0.3s;
+  &:target {
+    visibility: visible;
+    opacity: 1;
+    pointer-events: auto;
+  }
+  & > div {
+    width: 400px;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    padding: 2em;
+    background: white;
+  }
+  header {
+    font-weight: bold;
+  }
+  h1 {
+    font-size: 150%;
+    margin: 0 0 15px;
+  }
+}
+
+.modal-close {
+  color: #aaa;
+  line-height: 50px;
+  font-size: 80%;
+  position: absolute;
+  right: 0;
+  text-align: center;
+  top: 0;
+  width: 70px;
+  text-decoration: none;
+  &:hover {
+    color: black;
+  }
+}
+
+.lijevo h5 {
+  font-weight: 900;
+  font-size: 16px;
+  text-transform: uppercase;
+  color: #17171a;
+  text-align: left;
+  margin-left: 5px;
+}
+
+.desno {
+  text-align: right;
+}
 
 *,
 html,
@@ -261,6 +590,9 @@ body {
   gap: 16px;
   text-decoration: none;
   color: #333333;
+  background-color: rgb(231, 235, 255);
+  border: 2px solid rgb(0, 151, 254);
+  border-radius: 25px;
 }
 
 .currency-settings-div .settings-profile img {
@@ -334,6 +666,7 @@ import store from "@/store"
 import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 import "firebase/compat/firestore";
+import UploadImages from "vue-upload-drop-images";
 
 export default {
 name: 'settings',
@@ -342,6 +675,9 @@ data: function() {
     store
     }
 },
+ components: {
+                     UploadImages,
+                 },
 created(){
 setTimeout(() => {
 console.log(store.currentUser);
