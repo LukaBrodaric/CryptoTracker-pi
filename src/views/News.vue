83a60a273bffa29cf0b76ae7aefa5f7b2740a4da
unknown <brodaricluka81@gmail.com>
2022-02-02 15:09:35 +0100

    Update dokumentacija

--- src/views/News.vue
+++ src/views/News.vue
@@ -1,14 +1,19 @@
 <template>
-  <div>
-    <h1>This is an news page</h1>
-
-    <iframe
-      width="100%"
-      scrolling="no"
-      allowtransparency="true"
-      frameborder="0"
-      src="https://cryptopanic.com/widgets/news/?bg_color=FFFFFF&amp;currencies=BTC%2CETH%2CLTC%2CADA%2CBNB%2CSOL&amp;font_family=sans&amp;font_size=20&amp;header_bg_color=2C3E50&amp;header_text_color=FFFFFF&amp;link_color=0099FF&amp;news_feed=trending&amp;posts_limit=10&amp;text_color=2C3E50&amp;title=Trending%20news"
-      height="1000px"
-    ></iframe>
+  <div class="row">
+    <div class="col"></div>
+    <div class="col-8 mt-5 slika">
+      <img srcset="@/assets/white.png" style="width: 500px; height: 500px" />
+      <iframe
+        width="100%"
+        scrolling="no"
+        allowtransparency="true"
+        frameborder="0"
+        src="https://cryptopanic.com/widgets/news/?bg_color=FFFFFF&amp;currencies=BTC%2CETH%2CLTC%2CADA%2CBNB%2CSOL&amp;font_family=sans&amp;font_size=20&amp;header_bg_color=2C3E50&amp;header_text_color=FFFFFF&amp;link_color=000000&amp;news_feed=trending&amp;posts_limit=10&amp;text_color=2C3E50&amp;title=Trending%20news"
+        height="1000px"
+      ></iframe>
+    </div>
+    <div class="col"></div>
   </div>
 </template>
+
+<style scoped></style>
